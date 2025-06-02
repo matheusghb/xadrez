13b0034f048e093129bd6303486a9938e510e7db
Matheus Guilherme <mistermquack124@gmail.com>
2025-06-01 22:57:45 -0300

    socorro

--- xadrez.py
+++ xadrez.py
@@ -1,30 +1,10 @@
-def chgpc (posp, pos, ch, wflg):
-      t = ch[(pos*3)-3:(pos*3)]
-      pp = ch[(posp*3)-3:(posp*3)]
-      if t==('   '):
-            ch = ch[:(pos*3)-3]+pp+ch[(pos*3):]
-            ch = ch[:(posp*3)-3]+'   '+ch[posp*3:]
-            return ch
-      else:
-            ch = ch[:(pos*3)-3]+pp+ch[(pos*3):]
-            ch = ch[:(posp*3)-3]+'   '+ch[posp*3:]
-            return ch
-
-
 def jgdcor(c):
       if c==1:
             return "branco"
       else:
             return 'preto'
 
-def mtab(aa, ab, ac, ad, ae, af, ag, ah, 
-         ba, bb, bc, bd, be, bf, bg, bh, 
-         ca, cb, cc, cd, ce, cf, cg, ch,
-         da, db, dc, dd, de, df, dg, dh,
-         ea, eb, ec, ed, ee, ef, eg, eh, 
-         fa, fb, fc, fd, fe, ff, fg, fh,
-         ga, gb, gc, gd, ge, gf, gg, gh,
-         ha, hb, hc, hd, he, hf, hg, hh):
+def mtab(ch):
     print('    1     2      3     4     5     6     7     8 \n'
           '  -   -   -   -   -   -   -   -   -   -   -   -   -\n'
           '1 | %s | %s | %s | %s | %s | %s | %s | %s | \n'
@@ -35,14 +15,16 @@ def mtab(aa, ab, ac, ad, ae, af, ag, ah,
           '6 | %s | %s | %s | %s | %s | %s | %s | %s | \n'
           '7 | %s | %s | %s | %s | %s | %s | %s | %s | \n'
           '8 | %s | %s | %s | %s | %s | %s | %s | %s | \n'
-          '  -   -   -   -   -   -   -   -   -   -   -   -   -\n' %(aa, ab, ac, ad, ae, af, ag, ah, 
-                                                           ba, bb, bc, bd, be, bf, bg, bh,
-                                                           ca, cb, cc, cd, ce, cf, cg, ch,
-                                                           da, db, dc, dd, de, df, dg, dh,
-                                                           ea, eb, ec, ed, ee, ef, eg, eh,
-                                                           fa, fb, fc, fd, fe, ff, fg, fh,
-                                                           ga, gb, gc, gd, ge, gf, gg, gh,
-                                                           ha, hb, hc, hd, he, hf, hg, hh))
+          '  -   -   -   -   -   -   -   -   -   -   -   -   -\n' %(ch[0:3], ch[3:6], ch[6:9], ch[9:12], ch[12:15], ch[15:18], ch[18:21], ch[21:24], 
+                                                                  ch[24:27], ch[27:30], ch[30:33], ch[33:36],
+                                                                  ch[36:39], ch[39:42], ch[42:45], ch[45:48],
+                                                                  ch[48:51], ch[51:54], ch[54:57], ch[57:60], ch[60:63], ch[63:66], ch[66:69], ch[69:72],
+                                                                  ch[72:75], ch[75:78], ch[78:81], ch[81:84], ch[84:87], ch[87:90], ch[90:93], ch[93:96],
+                                                                  ch[96:99], ch[99:102], ch[102:105], ch[105:108], ch[108:111], ch[111:114], ch[114:117], ch[117:120],
+                                                                  ch[120:123], ch[123:126], ch[126:129], ch[129:132], ch[132:135], ch[135:138], ch[138:141], ch[141:144],
+                                                                  ch[144:147], ch[147:150], ch[150:153], ch[153:156], ch[156:159], ch[159:162], ch[162:165], ch[165:168],
+                                                                  ch[168:171], ch[171:174], ch[174:177], ch[177:180], ch[180:183], ch[183:186], ch[186:189], ch[189:192]
+                                                                  ))
 
 def checkp(a, b, chk):
       if len(a)<3:
@@ -55,7 +37,7 @@ def checkp(a, b, chk):
       else:
             return (((chk.find(a))//3)+1)
 
-def movp (a, b, pos):
+def movp (a, b, pos,pep):
 
       #a = peça
       #b = posição desejada
@@ -64,34 +46,29 @@ def movp (a, b, pos):
 
       pc = a[0]
       flg = 0 
-
       if a[2]==ch[(b*3)-1:(b*3)]:
             print('É uma peça de mesma cor.')
             flg=1
       else:
+            print (a, pep.find(a), pep)
             if pc == 'p': #peão
+                  p = 8
                   if a[2] == 'b':
-                        if b != (pos-8):
-                              if (b==(pos-7) and ch[((pos-7)*3)-3:((pos-7)*3)]!='   ') or (b==(pos-9) and ch[((pos-9)*3)-3:((pos-9)*3)]!='   '):
-                                    pass
-                              else:
-                                    flg = 1
-                        else:
-                              if (ch[((pos-8)*3)-3:((pos-8)*3)]!='   '):
-                                    print('A peça %s está no caminho.'%(ch[((pos-8)*3)-3:((pos-8)*3)]))
-                                    flg = 1
+                        p = p*(-1)
 
+                  if pep.find(a)==(-1) and (b==pos+(p*2)):
+                        return True
                   else:
-                        if b != (pos+8):
-                              if (b==(pos+7) and ch[((pos+7)*3)-3:((pos+7)*3)]!='   ') or (b==(pos+9) and ch[((pos+9)*3)-3:((pos+9)*3)]!='   '):
-                                    pass
+                        if b != pos+p:
+                              if (b==(pos+(p+1)) and ch[((pos+(p+1))*3)-3:((pos+(p+1))*3)]=='   ') and (b==(pos+(p-1)) and ch[((pos+(p-1))*3)-3:((pos+(p-1))*3)]=='   '):
+                                    flg = 1
                               else:
-                                    flg = 1
+                                    if b!=pos+p and b!=pos+(p-1) and b!=pos+(p+1):
+                                          flg = 1
                         else:
-                              if (ch[((pos+8)*3)-3:((pos+8)*3)]!='   '):
-                                    print('A peça %s está no caminho.'%(ch[((pos+8)*3)-3:((pos+8)*3)]))
+                              if (b==pos+p and (ch[((pos+p)*3)-3:((pos+p)*3)]!='   ')):
+                                    print('A peça %s está no caminho.'%(ch[((pos+p)*3)-3:((pos+p)*3)]))
                                     flg = 1
-
             
             elif pc == 'b': #bispo
                   i = 1
@@ -129,7 +106,7 @@ def movp (a, b, pos):
                                                 break 
 
                                     elif dire == 2: #diagonal superior direita
-                                          if ch[((pos+(-(7*j)))*3)-3:((pos+(-(7*j)))*3)] != '   ':
+                                          if ch[((pos+(-(7*j)))*3)-3:((pos+(-(7*j)))*3)] != '   ' :
                                                 print('A peça %s está no caminho.'%(ch[((pos+(-(7*j)))*3)-3:((pos+(-(7*j)))*3)]))
                                                 flg = 1
                                                 break
@@ -290,87 +267,24 @@ s=0
 c=1
 cor = ''
 wflg = 0
+r = 1
+cemb = 'Peças adquiridas: '
+cemp = 'Peças adquirias: '
+i = 2
+pep = ''
+
 while (s==0):
       if wflg == 1:
             print('O time %s venceu.'%(jgdcor(abs(c))))
             break
-
-      a1=ch[0:3] 
-      a2=ch[3:6]
-      a3=ch[6:9]
-      a4=ch[9:12]
-      a5=ch[12:15]
-      a6=ch[15:18]
-      a7=ch[18:21]
-      a8=ch[21:24]
-      b1=ch[24:27]
-      b2=ch[27:30]
-      b3=ch[30:33]
-      b4=ch[33:36]
-      b5=ch[36:39]
-      b6=ch[39:42]
-      b7=ch[42:45]
-      b8=ch[45:48]
-      c1=ch[48:51]
-      c2=ch[51:54]
-      c3=ch[54:57]
-      c4=ch[57:60]
-      c5=ch[60:63]
-      c6=ch[63:66]
-      c7=ch[66:69]
-      c8=ch[69:72]
-      d1=ch[72:75]
-      d2=ch[75:78]
-      d3=ch[78:81]
-      d4=ch[81:84]
-      d5=ch[84:87]
-      d6=ch[87:90]
-      d7=ch[90:93]
-      d8=ch[93:96]
-      e1=ch[96:99]
-      e2=ch[99:102]
-      e3=ch[102:105]
-      e4=ch[105:108]
-      e5=ch[108:111]
-      e6=ch[111:114]
-      e7=ch[114:117]
-      e8=ch[117:120]
-      f1=ch[120:123]
-      f2=ch[123:126]
-      f3=ch[126:129]
-      f4=ch[129:132]
-      f5=ch[132:135]
-      f6=ch[135:138]
-      f7=ch[138:141]
-      f8=ch[141:144]
-      g1=ch[144:147]
-      g2=ch[147:150]
-      g3=ch[150:153]
-      g4=ch[153:156]
-      g5=ch[156:159]
-      g6=ch[159:162]
-      g7=ch[162:165]
-      g8=ch[165:168]
-      h1=ch[168:171]
-      h2=ch[171:174]
-      h3=ch[174:177]
-      h4=ch[177:180]
-      h5=ch[180:183]
-      h6=ch[183:186]
-      h7=ch[186:189]
-      h8=ch[189:192]
-
       flg = 0
       cor = jgdcor(c)
-      print('\n\nTurno de %s.\n'%(cor))
-      mtab(a1,a2,a3,a4,a5,a6,a7,a8,
-            b1,b2,b3,b4,b5,b6,b7,b8,
-            c1,c2,c3,c4,c5,c6,c7,c8,
-            d1,d2,d3,d4,d5,d6,d7,d8,
-            e1,e2,e3,e4,e5,e6,e7,e8,
-            f1,f2,f3,f4,f5,f6,f7,f8,
-            g1,g2,g3,g4,g5,g6,g7,g8,
-            h1,h2,h3,h4,h5,h6,h7,h8)
+      if cor == 'branco':
+            cm = cemb
+      else:
+            cm = cemp
+      print('\n\nTurno de %s. rodada %i.\n%s'%(cor, r,cm))
+      mtab(ch)
       op = input('\nO que você deseja fazer?\n1 - Mover\n2 - Passar\n3 - Desistir\n4 - Sair\n=> ')
       s= 1
       if (op=='1'):
@@ -378,9 +292,10 @@ while (s==0):
             posp = checkp(pc, cor, ch)
             if (posp<0 or posp==False):
                   flg = 1
-                  print('teste 2')
                   print('\nCredênciais incorretas.')
             else:
+                  if pc[0]!='p':
+                        ch = ch.replace(' . ','   ')
                   pos = input('Digite onde você deseja posicionar essa peça (colunalinha): ')
                   try:
                         pos = (int(pos[0])+((int(pos[1])-1)*8))
@@ -388,9 +303,58 @@ while (s==0):
                         print ('Credênciais incorretas.')
                         c = abs(c-1)
                   else:
-                        chk = movp(pc, pos, posp)
+                        chk = movp(pc, pos, posp,pep)
+                        t = ch[(pos*3)-3:(pos*3)]
+                        pp = ch[(posp*3)-3:(posp*3)]
                         if chk == True:
-                              ch = chgpc(posp, pos, ch, wflg)
+                              
+                              en = ch[((ch.find(' . '))):(ch.find(' . '))+3]
+                              ch = ch.replace(' . ','   ')
+                              if (ch[(posp*3)-3:(posp*3)-2]=='p'):
+                                    if pep.find(pp)==(-1):
+                                          pep = pep+pp
+                                          p = 8
+                                          if pp[2]=='p':
+                                                p = p*(-1)
+                                          if posp+(-p) != pos:
+                                                ch = ch[:((pos+p)*3)-3]+' . '+ch[((pos+p)*3):]
+                              if (ch[(posp*3)-3:(posp*3)-2]=='p') and (pos<9 or pos>56):
+                                    i = i+1
+                                    flgu = 1
+                                    opc = ''
+                                    while flgu == 1:
+                                          flgu = 0
+                                          opc = str(input('Qual peça você deseja obter?\n1 - Torre\n2 - Cavalo\n3 - Bispo\n4 - Rainha\n=> '))
+
+                                          if opc=='1':
+                                                opc = 't'
+                                          elif opc=='2':
+                                                opc = 'c'
+                                          elif opc=='3':
+                                                opc = 'b'
+                                          elif opc=='4':
+                                                opc = 'R'
+                                          else:
+                                                print('Bote uma das opções acima.')
+                                                flgu = 1
+                                    ch = ch[:(posp*3)-3]+opc+str(i)+pp[2]+ch[(posp*3):]
+                              if t=='   ' or t==' . ':
+                                    if en==' . ' and t==' . ' and pp[0]=='p':
+                                          print('eu odeio luiz')
+                                          print((((pos+(-(p*2)))*3))-3, ((pos+(-(p*2)))*3))
+                                          ch = ch[:(((pos+(-(p)))*3))-3]+'   '+ch[((pos+(-(p)))*3):]
+
+                                    ch = ch[:(pos*3)-3]+pp+ch[(pos*3):]
+                                    ch = ch[:(posp*3)-3]+'   '+ch[posp*3:]
+                              else:
+                                    if ch[(pos*3)-1:(pos*3)]=='b':
+                                          cemp = cemp+ch[(pos*3)-3:(pos*3)]+', '
+                                    else:
+                                          cemb = cemb+ch[(pos*3)-3:(pos*3)]+', '
+                                    ch = ch[:(pos*3)-3]+pp+ch[(pos*3):]
+                                    ch = ch[:(posp*3)-3]+'   '+ch[posp*3:]
+            
+                              r = r+abs(c-1)
                               if ch.find('r1b')==-1 or ch.find('r1p')==-1:
                                     wflg = 1
                         else:
@@ -398,8 +362,9 @@ while (s==0):
                               c = abs(c-1)
       if (op=='2'):
             s=0
+            r = r+abs(c-1)
       elif (op=='3'):
-            print('O time %s venceu.'%(jgdcor(abs(c-1))))
+            print('O time %s venceu em %i rodadas.'%(jgdcor(abs(c-1)), r))
             break
       elif (op=='4'):
             print('\nFechando o programa.')
